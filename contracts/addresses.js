761fb6e830605c61a212d497e03343a8a740d6ba
Jorge Alcazar Diaz <80380017+alcazar9491@users.noreply.github.com>
2023-09-29 00:50:40 -0700

    ready to deploy

---
+++ contracts/addresses.js
@@ -0,0 +1,15 @@
+
+ const addresses = {
+   endergyEndEth: {
+        80001:'',
+        137:'0x97D57A8C33a131f5805D6D1CeD1C34c21a823121'
+    },
+   endergyEndEnd: {
+        80001:'',
+        137:'0x4D04c744f6B7AC2133BD4b43de98D91A2b32ec33'
+    },
+}
+
+module.exports = {
+    addresses
+}
---
+++ services/endergy.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+const web3contract  = require('./web3Intancer')
+const abiEnd = require('../contracts/abis/endergyEND.json')
+const abiEth = require('../contracts/abis/endergyETH.json')
+
+const {addresses} = require('../contracts/addresses')
+
+
+
+
+ 
+ const endergyData = async ( web3 ) => {
+
+    const contractEnd = await web3contract.getContract(abiEnd, addresses.endergyEndEnd[process.env.CHAIN_ID], web3)
+    const contractEth = await web3contract.getContract(abiEth, addresses.endergyEndEth[process.env.CHAIN_ID], web3)
+    
+
+    let totalStakedAmountEth = 0;
+    let rewardPerBlockEth = 0;
+    let rewardBalanceEth = 0;
+
+    let totalStakedAmountEnd = 0;
+    let rewardPerBlockEnd = 0;
+    let rewardBalanceEnd = 0;
+
+    try {
+
+        totalStakedAmountEnd = await contractEnd?.methods.totalStakedAmount().call();
+        rewardPerBlockEnd = await contractEnd.methods.rewardPerBlock().call();
+        rewardBalanceEnd = await contractEnd.methods.getRewardBalance().call();
+
+        totalStakedAmountEth = await contractEth?.methods.totalStakedAmount().call();
+        rewardPerBlockEth = await contractEth.methods.rewardPerBlock().call();
+        rewardBalanceEth = await contractEth.methods.getRewardBalance().call();
+
+    } catch (error) {
+        console.log(error)
+    }
+    
+
+
+    return {
+
+        endergyEth:{
+            totalStakedAmountEth:totalStakedAmountEth.toString(),
+            rewardPerBlockEth:rewardPerBlockEth.toString(),
+            rewardBalanceEth:rewardBalanceEth.toString()
+        },
+        endergyEnd:{
+            totalStakedAmountEnd:totalStakedAmountEnd.toString(),
+            rewardPerBlockEnd:rewardPerBlockEnd.toString(),
+            rewardBalanceEnd:rewardBalanceEnd.toString()
+        }
+    }
+
+}
+
+
+
+module.exports = {
+    endergyData
+}
